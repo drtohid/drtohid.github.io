fbb20fc8db6c15f065162e48352c59364909d08b
Abdullah Tohid <abdullahtohid280091994@gmail.com>
2025-08-29 04:48:32 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react( ),tailwindcss()],
-  base: './', // <--- CHANGE THIS LINE
+  plugins: [react( ), tailwindcss()],
+  base: '/', // Correct for a `username.github.io` repository
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
